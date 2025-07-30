71cd188fc2546021612815c14d0000c7ae200555
Lilcooldoode <lilcooldoode@gmail.com>
2025-07-30 21:05:27 +0100

    Create CanFlySpells.cs

---
+++ PlayerAgents/CanFlySpells.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Shared;
+
+public static class CanFlySpells
+{
+    public static readonly HashSet<Spell> List = new()
+    {
+        Spell.ElementalShot,
+        Spell.FireBall,
+        Spell.GreatFireBall,
+        Spell.FrostCrunch,
+        Spell.SoulFireBall,
+        Spell.CatTongue,
+        Spell.StraightShot,
+        Spell.DoubleShot,
+        Spell.DelayedExplosion,
+        Spell.BindingShot,
+        Spell.VampireShot,
+        Spell.PoisonShot,
+        Spell.CrippleShot,
+        Spell.NapalmShot,
+        Spell.SummonVampire,
+        Spell.SummonToad,
+        Spell.SummonSnakes,
+        Spell.Stonetrap,
+        Spell.Portal,
+        Spell.FireBounce,
+        Spell.MeteorShower
+    };
+}
